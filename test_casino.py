f754d18f4ffe3b08507b925a88a9cc8215e322f0
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2021-03-16 16:31:09 +0100

    Update test_casino.py

--- test_casino.py
+++ test_casino.py
@@ -1,14 +1,14 @@
 #import tp_casino.py
-from tp_casino import *
-#from tp_casino import Casino
-#from tp_casino import getArgentMise
-#from tp_casino import getNumeroMise
+from tp_casino import Joueur
+from tp_casino import Casino
+from tp_casino import getArgentMise
+from tp_casino import getNumeroMise
 
 j1 = Joueur("The Mike", 2000)
 c1 = Casino("Casa Blanca", j1)
 
 def test_create_user():
-    assert j1 != null,"le joueur n'a pas été crée"
+    assert j1 != None, "le joueur n'a pas été crée"
     
 
 def test_argent_positif():
@@ -16,8 +16,8 @@ def test_argent_positif():
 
 
 def test_creer_casino():
-    assert j1 != null, "le casino n'a pas de joueur"
-    assert c1 != null, "le casino n'a pas été crée "
+    assert j1 != None, "le casino n'a pas de joueur"
+    assert c1 != None, "le casino n'a pas été crée "
 
 def test_argent_mise():
 
@@ -30,5 +30,5 @@ def test_argent_mise():
 def test_numero_mise():
     numeroMise = 1
     numeroMise = getNumeroMise(numeroMise)
-    assert numeroMise <= 49,"Numero mise en dehors de l intervalle 0-49/ Supérieur à 49 !" 
-    assert numeroMise >= 0, "Numero mise en dehors de l intervalle 0-49/ Négatif!"
+    assert numeroMise <= 49
+    assert numeroMise >= 0, "Numero mise en dehors de l intervalle 0-49 !"
