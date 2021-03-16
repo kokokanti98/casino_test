dbb82c91e8a3d68f0491a4042613af6a18afe0d3
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2021-03-16 16:03:07 +0100

    Update test_casino.py

--- test_casino.py
+++ test_casino.py
@@ -1,59 +1,33 @@
-from requests import get
-from bs4 import BeautifulSoup
-import datetime
-import time
-from os import system, name
-from termcolor import colored
-import winsound
+#import tp_casino.py
+from tp_casino import *
+#from tp_casino import Casino
+#from tp_casino import getArgentMise
+#from tp_casino import getNumeroMise
 
-url = "https://www.amazon.fr/dp/B08HBQWBHH/"  # le lien de la 3090
-url2 = "https://www.amazon.fr/dp/B013M1QN9K/"  # le lien de card dispo
+j1 = Joueur("The Mike", 2000)
+c1 = Casino("Casa Blanca", j1)
 
-headers = {"USER-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 OPR/74.0.3911.160"}
+def test_create_user():
+    assert j1 != null,"le joueur n'a pas été crée"
+    
 
-
-def availability():
-    while True:
-        page = get(url2, headers=headers)
-        soup = BeautifulSoup(page.content, "html.parser")
-        titre = soup.find(id="productTitle").get_text().strip()
-        #print(titre.strip()) Nvidia Graphic Card K1200
-        avaibility = soup.find(id="availability").get_text().strip()[0:31]
-        #print(avaibility) #Il ne reste plus que 2 exemplaire(s) en stock.
-        prix = soup.find(id="priceblock_ourprice")
-        if(prix is None):
-            prix = "0"
-        else:
-            prix = prix.get_text()
-        #print(prix.strip()) 498,00 €
-        if(avaibility == 'Voir les offres de ces vendeurs'):
-            affichage(titre, avaibility, prix, "red")
-            seconde = 60
-            now = datetime.datetime.now()
-            now = now.strftime("%H:%M:%S")
-            print(f"{now} prochaine actualisation dans {seconde} secondes...")
-            time.sleep(seconde)
-            continue
-        else:
-            affichage(titre, avaibility, prix, "green")
-            jouerSon()
-            break
+def test_argent_positif():
+    assert j1.argent >= 0, "l'argent de l'utilisateur est  négatif "
 
 
-def affichage(titre, availability, prix, color):
-    clear()
-    print(colored(titre + "\n " + availability + " \n" + prix, color))
+def test_creer_casino():
+    assert j1 != null, "le casino n'a pas de joueur"
+    assert c1 != null, "le casino n'a pas été crée "
 
+def test_argent_mise():
 
-def clear():
-    if name == 'nt':
-        _ = system('cls')
-    else:
-        _ = system('clear')
+    argentMise = 1
+    argentMise = getArgentMise(argentMise, j1)
+    assert argentMise >= 0,"Argent mise négatif !"
+    assert argentMise > j1.argent, "Argent misé superieur à l'argent du joueur !"
+    
 
-
-def jouerSon():
-    winsound.PlaySound("SystemQuestion", winsound.SND_ALIAS)
-
-
-availability()
+def test_numero_mise():
+    numeroMise = 1
+    numeroMise = getNumeroMise(numeroMise)
+    assert numeroMise <= 49 && numeroMise >= 0, "Numero mise en dehors de l intervalle 0-49 !"
